f684342cad89d4c2989dfcb82fedf05df4752b2a
Konstantin <kot199217@mail.ru>
2022-09-25 19:38:59 +0900

    Created Zadanie1

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void Polindrom()
+{
+    int NumberOne(int num, int Numbers)
+    {
+        while (num / Math.Pow(10, Numbers) > 1)
+        {
+            num /= 10;
+        }
+        return num;
+    }
+    Console.WriteLine("Введите пятизначное число");
+    int number = int.Parse(Console.ReadLine());// 1234265735
+    if (number < 0) number *= -1;
+    if (number > 9999)
+    {
+        int x = number % 100;
+        int y = NumberOne(number, 2);
+        if (x / 10 == y % 10 && x % 10 == y / 10)
+        {
+            Console.WriteLine("Полиндром");
+        }
+        else
+        {
+            Console.WriteLine("Не Полиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введите число больше ");
+        Polindrom();
+    }
+}
+
+Polindrom();
